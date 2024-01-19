778463982079fb1dee50c9e0f09c56d704908916
Bharathi V <128215224+Bharathiv1403@users.noreply.github.com>
2024-01-19 09:03:05 +0530

    Update email find.py

--- re/email find.py
+++ re/email find.py
@@ -2,7 +2,8 @@
 import re
 str = input("Enter the Gmail : ")
 match = re.search(r'\w+@\w+', str)
+match1=re.search(r'\b\w*gmail\w*\b',str)
 if match:
-    print("Your Gmail is Correct ",match.group()) 
+    print("Your Gmail is Correct ",match and match1.group()) 
 else:
     print("Not Match")
